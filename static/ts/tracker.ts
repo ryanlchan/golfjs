5c3042aa9a8a2f1f621652619b9ce6a9aa8b671d
Ryan Chan <ryan@ryanlchan.com>
2023-08-13 17:24:19 -0700

    Add a popup to current position marker indicating distance to pin

--- static/ts/tracker.ts
+++ static/ts/tracker.ts
@@ -1415,12 +1415,14 @@ function currentPositionUpdate() {
         if (currentPositionMarker) {
             // If the marker already exists, just update its position
             currentPositionMarker.setLatLng(latlong);
+            currentPositionMarker.getPopup().update();
         } else {
             // Create a new marker and add it to the map
             currentPositionMarker = L.circleMarker(
                 latlong,
                 { radius: 10, fillColor: "#4A89F3", color: "#FFF", weight: 1, opacity: 0.8, fillOpacity: 0.8 }
             );
+            currentPositionMarker.bindPopup(positionMarkerPopupText)
             layerCreate(markerID, currentPositionMarker);
         }
 
@@ -1431,6 +1433,15 @@ function currentPositionUpdate() {
         timeout: 10000,
         maximumAge: 1000
     });
+}
+
+function positionMarkerPopupText(layer: L.Marker) {
+    if (!currentHole) return "";
+    const latlng = layer.getLatLng();
+    const coord = { x: latlng["lng"], y: latlng["lat"], crs: "EPSG:4236" }
+    const dOpt = { to_unit: displayUnits, include_unit: true }
+    const dist = formatDistance(getDistance(coord, currentHole.pin), dOpt);
+    return `${dist} to pin`;
 }
 
 /**
