3da5110f3c096c0a51b55859d45f3972c67e3a76
Ryan Chan <ryan@ryanlchan.com>
2023-11-21 11:12:16 -0800

    Add club selection/customization

---
+++ static/ts/clubs.ts
@@ -0,0 +1,71 @@
+import * as cache from "./cache";
+import { typeid } from "typeid-js";
+
+export class GolfClub {
+    id: string;
+    name: string; // actually more like type
+    dispersion: number;
+    displayName?: string;
+    class?: string;
+
+    constructor() {
+        this.id = typeid("club").toString();
+    }
+}
+
+/**
+ * Lookup function to get all default clubs, currently static
+ * @returns {GolfClub[]}
+ */
+function getDefaultClubs(): GolfClub[] {
+    return [
+        { id: "1", name: "D", dispersion: 30 },
+        { id: "2", name: "3w", dispersion: 27 },
+        { id: "3", name: "3h", dispersion: 25.5 },
+        { id: "4", name: "4i", dispersion: 22 },
+        { id: "5", name: "5i", dispersion: 20 },
+        { id: "6", name: "6i", dispersion: 17 },
+        { id: "7", name: "7i", dispersion: 16 },
+        { id: "8", name: "8i", dispersion: 13.5 },
+        { id: "9", name: "9i", dispersion: 11.5 },
+        { id: "10", name: "Pw", dispersion: 10 },
+        { id: "11", name: "Aw", dispersion: 7.5 },
+        { id: "12", name: "Sw", dispersion: 6 },
+        { id: "13", name: "Lw", dispersion: 5 },
+        { id: "14", name: "P", dispersion: -0.15 },
+    ]
+}
+
+/**
+ * Get all user-settable golf clubs
+ * @returns {GolfClub[]} An array of all user-set golf clubs
+ */
+export function getUserClubs(): GolfClub[] {
+    let settings = cache.getJSON('settings') || {};
+    let clubs = settings["clubs"];
+    return clubs || [];
+}
+
+/**
+ * Lookup function to get all usable clubs, including non-user set penalty/undeclared clubs
+ * @returns {GolfClub[]} an array of all usable Golf Clubs
+ */
+export function getUsableClubs(): GolfClub[] {
+    let clubs = getUserClubs();
+    if (!clubs || clubs.length == 0) {
+        clubs = getDefaultClubs();
+    }
+    clubs.push({ id: "15", name: "Penalty", dispersion: 1, class: "danger" })
+    clubs.push({ id: "16", name: "Skip", dispersion: 1, class: "secondary" })
+    return clubs;
+}
+
+/**
+ * Persist a set of user-defined clubs
+ * @param {GolfClub[]} clubs an array of golf club objects
+ */
+export function saveUserClubs(clubs: GolfClub[]) {
+    const oldSettings = cache.getJSON('settings') || {};
+    const settings = { ...oldSettings, clubs }
+    cache.setJSON("settings", settings);
+}
