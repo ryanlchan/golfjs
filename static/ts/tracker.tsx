9dd3abbe2fed08cf12c2f98844615e3166928876
Ryan Chan <ryan@ryanlchan.com>
2023-11-28 22:56:55 -0800

    Convert over-Map controls to JSX

--- static/ts/tracker.tsx
+++ static/ts/tracker.tsx
@@ -10,6 +10,7 @@ import { Loader } from "@googlemaps/js-api-loader";
 import "./googlemutant.js";
 import { typeid } from "typeid-js";
 import { render, VNode } from 'preact';
+import { useState } from 'preact/hooks';
 
 // Modules
 import * as grids from "./grids.js";
@@ -376,9 +377,6 @@ function strokeMarkerDeactivate(e?) {
  * Create an aim marker where the user has currently clicked
  */
 function strokeMarkerAimCreate() {
-    // Show the set Aim button
-    strokeMarkerAimCreateButton.classList.remove("inactive")
-
     if (!activeStroke) {
         console.error("Cannot add aim, no active stroke")
         return
@@ -440,9 +438,6 @@ function strokeMarkerAimUpdate() {
  * Delete the current active stroke's aim marker, ring, and grid
  */
 function strokeMarkerAimDelete() {
-    // Hide Aim button
-    strokeMarkerAimCreateButton.classList.add("inactive")
-
     // Hide aim layers
     layerDelete("active_aim");
     layerDelete("active_aim_ring");
@@ -1275,7 +1270,7 @@ function currentPositionUpdate() {
         }
 
         // Update live distance box
-        distanceToPinViewUpdate();
+        upperMapControlsUpdate();
     }, showPositionError, {
         enableHighAccuracy: true,
         timeout: 5000,
@@ -1296,6 +1291,147 @@ function positionMarkerPopupText(layer: L.Marker) {
  * View components
  */
 
+interface AppMenuItem { href: string, icon: VNode, text: string }
+function AppMenuItem(props: AppMenuItem) {
+    return <a href={props.href}><span className="menuIcon">{props.icon}</span>{props.text}</a>
+}
+
+function AppMenuNewLink() {
+    const href = "new.html";
+    const icon = <svg xmlns="http://www.w3.org/2000/svg" height="16" width="12" viewBox="0 0 384 512"><path d="M384 192c0 66.8-34.1 125.6-85.8 160H85.8C34.1 317.6 0 258.8 0 192C0 86 86 0 192 0S384 86 384 192zM242.1 256.6c0 18.5-15 33.5-33.5 33.5c-4.9 0-9.1 5.1-5.4 8.4c5.9 5.2 13.7 8.4 22.1 8.4c18.5 0 33.5-15 33.5-33.5c0-8.5-3.2-16.2-8.4-22.1c-3.3-3.7-8.4 .5-8.4 5.4zm-52.3-49.3c-4.9 0-9.1 5.1-5.4 8.4c5.9 5.2 13.7 8.4 22.1 8.4c18.5 0 33.5-15 33.5-33.5c0-8.5-3.2-16.2-8.4-22.1c-3.3-3.7-8.4 .5-8.4 5.4c0 18.5-15 33.5-33.5 33.5zm113.5-17.5c0 18.5-15 33.5-33.5 33.5c-4.9 0-9.1 5.1-5.4 8.4c5.9 5.2 13.7 8.4 22.1 8.4c18.5 0 33.5-15 33.5-33.5c0-8.5-3.2-16.2-8.4-22.1c-3.3-3.7-8.4 .5-8.4 5.4zM96 416c0-17.7 14.3-32 32-32h64 64c17.7 0 32 14.3 32 32s-14.3 32-32 32H240c-8.8 0-16 7.2-16 16v16c0 17.7-14.3 32-32 32s-32-14.3-32-32V464c0-8.8-7.2-16-16-16H128c-17.7 0-32-14.3-32-32z" /></svg>
+    const text = "New round"
+    return <AppMenuItem href={href} text={text} icon={icon} />
+}
+
+function AppMenuStatsLink() {
+    const href = "stats.html";
+    const icon = <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path d="M64 64c0-17.7-14.3-32-32-32S0 46.3 0 64V400c0 44.2 35.8 80 80 80H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H80c-8.8 0-16-7.2-16-16V64zm406.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L320 210.7l-57.4-57.4c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L240 221.3l57.4 57.4c12.5 12.5 32.8 12.5 45.3 0l128-128z" /></svg>
+    const text = "Stats"
+    return <AppMenuItem href={href} text={text} icon={icon} />
+}
+
+function AppMenuSettingsLink() {
+    const href = "settings.html";
+    const icon = <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path d="M495.9 166.6c3.2 8.7 .5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3 39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1 31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2 17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4L83.1 425.9c-8.8 2.8-18.6 .3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4C64.6 273.1 64 264.6 64 256s.6-17.1 1.7-25.4L22.4 191.2c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1 9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7 8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336a80 80 0 1 0 0-160 80 80 0 1 0 0 160z" /></svg>
+    const text = "Settings"
+    return <AppMenuItem href={href} text={text} icon={icon} />
+}
+
+function AppMenu() {
+    return <div className="appMenu">
+        <AppMenuNewLink />
+        <AppMenuStatsLink />
+        <AppMenuSettingsLink />
+    </div>
+}
+
+function MenuButton() {
+    const [menuVisible, setMenuVisible] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuVisible(!menuVisible);
+    };
+
+    return (<div class="menuButton">
+        <button id="menuButton" className="mapButton" onClick={toggleMenu}>
+            <svg height="1.25em" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M4 18L20 18" stroke="#000000" stroke-width="2" stroke-linecap="round" />
+                <path d="M4 12L20 12" stroke="#000000" stroke-width="2" stroke-linecap="round" />
+                <path d="M4 6L20 6" stroke="#000000" stroke-width="2" stroke-linecap="round" />
+            </svg>
+        </button>
+        {menuVisible && <AppMenu />}
+    </div>);
+}
+
+function MapRecenterButton() {
+    const onClick = () => mapRecenter();
+    return (<button id="recenter" className="mapButton" onClick={onClick}>
+        <svg xmlns="http://www.w3.org/2000/svg" height="1.25em" viewBox="0 0 448 512"><path d="M429.6 92.1c4.9-11.9 2.1-25.6-7-34.7s-22.8-11.9-34.7-7l-352 144c-14.2 5.8-22.2 20.8-19.3 35.8s16.1 25.8 31.4 25.8H224V432c0 15.3 10.8 28.4 25.8 31.4s30-5.1 35.8-19.3l144-352z" />
+        </svg>
+    </button>);
+}
+
+function StrokeAddButton() {
+    const handleStrokeAddClick = () => {
+        clubStrokeViewToggle();
+        strokeMarkerDeactivate();
+    }
+    return (<button id="strokeAdd" className="success mapButton" onClick={handleStrokeAddClick}>
+        <svg xmlns="http://www.w3.org/2000/svg" height="1.25em" viewBox="0 0 448 512">
+            <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
+        </svg>
+    </button>);
+}
+
+function StrokeAimResetButton() {
+    const classes = ["mapButton", activeStroke ? "" : "inactive"];
+    const onClick = () => {
+        strokeAimReset(activeStroke);
+        rerender("full");
+    }
+    return (<button id="strokeAimReset" className={classes.join(' ')} onClick={onClick}>
+        <svg xmlns="http://www.w3.org/2000/svg" height="1.25em" viewBox="0 0 512 512">
+            <path d="M48 24C48 10.7 37.3 0 24 0S0 10.7 0 24V64 350.5 400v88c0 13.3 10.7 24 24 24s24-10.7 24-24V388l80.3-20.1c41.1-10.3 84.6-5.5 122.5 13.4c44.2 22.1 95.5 24.8 141.7 7.4l34.7-13c12.5-4.7 20.8-16.6 20.8-30V66.1c0-23-24.2-38-44.8-27.7l-9.6 4.8c-46.3 23.2-100.8 23.2-147.1 0c-35.1-17.6-75.4-22-113.5-12.5L48 52V24zm0 77.5l96.6-24.2c27-6.7 55.5-3.6 80.4 8.8c54.9 27.4 118.7 29.7 175 6.8V334.7l-24.4 9.1c-33.7 12.6-71.2 10.7-103.4-5.4c-48.2-24.1-103.3-30.1-155.6-17.1L48 338.5v-237z" />
+        </svg>
+    </button>);
+}
+
+interface DistanceTrackerProps { location: Coordinate, name: string }
+function DistanceTracker(props: DistanceTrackerProps) {
+    const active = currentHole?.pin && currentPositionRead();
+    if (!active) return;
+    const opt = { to_unit: displayUnits, include_unit: true };
+    const pos = currentCoordRead();
+    const dist = formatDistance(getDistance(pos, currentHole.pin), opt);
+    const id = `distanceTo${name}Container`;
+    return (<div id={id} className="mapInfoBox">
+        <span>{props.name}</span>
+        <div id="distanceToPin">
+            {dist}
+        </div>
+    </div>);
+}
+
+function PinDistanceTracker() {
+    const pinCoord = currentHole?.pin;
+    const name = "Pin"
+    return <DistanceTracker location={pinCoord} name={name} />
+}
+
+function MapControlsUpperRight() {
+    return <div id="mapControlsUpperRight" className="mapControlsContainer">
+        <MenuButton />
+        <PinDistanceTracker />
+    </div>
+}
+
+function MapControlsRight() {
+    return (<div class="mapControlsContainer" id="mapControlsRight">
+        <MapRecenterButton />
+        <StrokeAddButton />
+    </div>)
+}
+
+function MapControlsLeft() {
+    return (<div class="mapControlsContainer" id="mapControlsLeft">
+        <StrokeAimResetButton />
+    </div>)
+}
+
+function MapControlsLower() {
+    return (<div id="mapControlsWrapper">
+        <MapControlsRight />
+        <MapControlsLeft />
+    </div>);
+}
+
+function MapControlsUpper() {
+    return <>
+        <MapControlsUpperRight />
+    </>
+}
+
 interface HoleInfoProps { hole: Hole, round: Round }
 function HoleInfo(props: HoleInfoProps) {
     const round = props.round
@@ -1315,8 +1451,8 @@ function HoleInfo(props: HoleInfoProps) {
 /**
  * Update a given select element with current hole options
  * @param {number} props.currentHoleIndex
- * @param {Hole[]} props.holes
- */
+    * @param {Hole[]} props.holes
+    */
 interface HoleSelectorProps { currentHoleIndex: number, holes: Hole[] }
 function HoleSelector(props: HoleSelectorProps) {
     const handleSelect = (e) => holeSelect(parseInt(e.target.value));
@@ -1351,8 +1487,8 @@ function HoleControls(props: HoleControlsProps) {
 /**
  * Create a list item for the Stroke Stats list
  * @param {Stroke} props.stroke
- * @returns {HTMLElement} the li element for the list
- */
+    * @returns {HTMLElement} the li element for the list
+    */
 interface StrokeStatsListItemProps { stroke: Stroke }
 function StrokeStatsListItem(props: StrokeStatsListItemProps) {
     const stroke = props.stroke;
@@ -1386,7 +1522,7 @@ function StrokeStatsListItem(props: StrokeStatsListItemProps) {
 /**
  * Generate a list of strokes with controls to adjust them
  * @param {Stroke[]} props.strokes
- */
+    */
 interface StrokeStatsListProps { strokes: Stroke[] }
 function StrokeStatsList(props: StrokeStatsListProps) {
     return (<div id="strokeList"><ol>
@@ -1458,7 +1594,7 @@ function GridTypeControl() {
 }
 
 interface DispersionControlProps { stroke: Stroke }
-function DispersionControl(props) {
+function DispersionControl(props: DispersionControlProps) {
     const id = "dispersionControlContainer";
     return <div className="buttonRow" id={id}>
         <label for={id}>Dispersion:</label>
@@ -1571,7 +1707,7 @@ function Scorecard(props: ScorecardProps) {
  * Return the score class (birdie, bogey, etc)
  * @param relativeScore the score relative to par
  * @returns {string} the score class
-        */
+    */
 function scoreClass(relativeScore: number): string {
     const s = Math.round(relativeScore);
     if (s >= 2) {
@@ -1600,11 +1736,18 @@ function StrokeAndHoleControls(props: StrokeAndHoleControlsProps) {
     </div>
 }
 
+function SubMapControls() {
+    return (<>
+        <MapControlsLower />
+        <StrokeAndHoleControls activeStroke={activeStroke} hole={currentHole} round={round} />
+    </>)
+}
+
 /**
  * Create a link that deletes this stroke
  * @param {Stroke} stroke
- * @returns {HTMLElement}
- */
+    * @returns {HTMLElement}
+    */
 interface StrokeDeleteButtonProps { stroke: Stroke }
 function StrokeDeleteButton(props: StrokeDeleteButtonProps): VNode {
     const icon = <span>&#215;</span>;
@@ -1618,9 +1761,9 @@ function StrokeDeleteButton(props: StrokeDeleteButtonProps): VNode {
 /**
  * Create a link that moves this stroke
  * @param {Stroke} stroke the stroke to move
- * @param {Number} offset the offset for the stroke index
- * @returns {HTMLElement}
- */
+    * @param {Number} offset the offset for the stroke index
+    * @returns {HTMLElement}
+    */
 interface StrokeMoveButtonProps { stroke: Stroke, offset: number }
 function StrokeMoveButton(props: StrokeMoveButtonProps): VNode {
     const stroke = props.stroke;
@@ -1632,33 +1775,23 @@ function StrokeMoveButton(props: StrokeMoveButtonProps): VNode {
     return <button onClick={clickHandler}>{icon}</button>
 }
 
-function distanceToPinViewUpdate(id: string = "distanceToPin"): void {
-    const el = document.getElementById(id);
-    const parent = el.parentElement;
-    if (currentHole?.pin && currentPositionRead()) {
-        parent.classList.remove("inactive");
-
-        const opt = { to_unit: displayUnits, include_unit: true };
-        const pos = currentCoordRead();
-        const dist = formatDistance(getDistance(pos, currentHole.pin), opt);
-        el.innerText = dist;
-    } else {
-        parent.classList.add("inactive");
-    }
-}
-
 /**
  * Rerendering handlers
  */
-function strokeAndHoleControlsUpdate() {
+function subMapControlsUpdate() {
     const el = document.getElementById("subMapControls");
-    render(<StrokeAndHoleControls activeStroke={activeStroke} hole={currentHole} round={round} />, el);
+    render(<SubMapControls />, el);
+}
+
+function upperMapControlsUpdate() {
+    const el = document.querySelector('div#upperMapControls');
+    render(<MapControlsUpper />, el);
 }
 
 /**
  * Rerender key views based on volatile data
  * @param {string} category the category of rerender to perform.
-        */
+    */
 function rerender(category: string = "map") {
     const lines = () => {
         strokelineUpdate();
@@ -1671,14 +1804,15 @@ function rerender(category: string = "map") {
         }
     }
     const controls = () => {
-        strokeAndHoleControlsUpdate();
+        subMapControlsUpdate();
+        upperMapControlsUpdate();
         saveData();
     }
     const activeGrids = () => {
         if (!activeStroke) return
         gridUpdate().then(() => {
             strokeMarkerAimUpdate();
-            strokeAndHoleControlsUpdate();
+            subMapControlsUpdate();
         }).catch((error) => console.error(error));
     }
     const activeStates = () => {
@@ -1699,8 +1833,8 @@ function rerender(category: string = "map") {
 /**
  * Render a set of Club buttons into an HTML element based on an array of Club objects
  * @param {Array} clubs
-        * @param {HTMLElement} targetElement
-        */
+    * @param {HTMLElement} targetElement
+    */
 const clubDataFields = ["dispersion"]
 function clubStrokeViewCreate(clubs, targetElement) {
     clubs.forEach((clubData) => {
@@ -1735,8 +1869,8 @@ function clubStrokeViewCreate(clubs, targetElement) {
 /**
  * Handle a click on a club stroke create button
  * @param {Club} club
-        * @returns {function}
-        */
+    * @returns {function}
+    */
 function clubStrokeCreateCallback(club: Club): () => void {
     return (() => {
         clubStrokeViewToggle();
@@ -1773,30 +1907,6 @@ function handleLoad() {
     });
 }
 
-/**
- * Handles the click event for logging the current location.
- */
-function handleStrokeAddClick() {
-    clubStrokeViewToggle();
-    strokeMarkerDeactivate();
-}
-
-/**
- * If the user is not in the current course, allow them to click the screen to
- * set a new stroke's location
- */
-function handleStrokeMarkerAimResetClick() {
-    strokeAimReset(activeStroke);
-    rerender("full");
-}
-
-/**
- * Recenter the map on the current hole
- */
-function handleRecenterClick() {
-    mapRecenter();
-}
-
 function handleHoleIncrement(incr) {
     let curHoleNum = -1;
     if (currentHole) {
@@ -1805,9 +1915,9 @@ function handleHoleIncrement(incr) {
     curHoleNum += incr;
 
     if (curHoleNum >= round.holes.length) {
-        curHoleNum = round.holes.length - 1;
-    } else if (curHoleNum < -1) {
         curHoleNum = -1;
+    } else if (curHoleNum < -1) {
+        curHoleNum = round.holes.length - 1;
     }
     holeSelect(curHoleNum);
 }
@@ -1835,11 +1945,7 @@ function showPositionError(error: PositionError) {
 }
 
 // Event listeners
-let strokeMarkerAimCreateButton = document.getElementById("strokeAimReset")
 
 window.addEventListener('load', handleLoad);
-document.getElementById("strokeAdd").addEventListener("click", handleStrokeAddClick);
 document.getElementById("clubStrokeCreateContainerClose").addEventListener("click", clubStrokeViewToggle);
-document.getElementById("recenter").addEventListener("click", handleRecenterClick);
-strokeMarkerAimCreateButton.addEventListener('click', handleStrokeMarkerAimResetClick);
 document.getElementById("panicButton").addEventListener("click", () => { throw new Error("PANIC!!!") });
