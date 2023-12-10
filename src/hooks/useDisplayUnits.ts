c82eb0d63283304ee0b14e35e56fd0aefa36a9c5
Ryan Chan <ryan@ryanlchan.com>
2023-12-10 10:50:23 -0800

    Add useDistanceOptions hook

--- src/hooks/useDisplayUnits.ts
+++ src/hooks/useDisplayUnits.ts
@@ -1,3 +1,4 @@
+import { formatDistanceOptions } from 'common/projections';
 import { AppContext } from 'contexts/appContext';
 import { useContext } from 'preact/hooks';
 
@@ -5,4 +6,16 @@ export const useDisplayUnits = () => {
     const appState = useContext(AppContext);
     return appState?.settingsStore?.data.value?.displayUnits || "yards";
 };
+
+/**
+ * Pre-generate a formatDistanceOptions obj from context
+ * Default value: { to_unit: displayUnits, precision: 1, include_unit: true};
+ * @param options overrides of the default options
+ * @returns {formatDistanceOptions}
+ */
+export const useDistanceOptions = (options?: formatDistanceOptions) => {
+    const displayUnits = useDisplayUnits();
+    return { to_unit: displayUnits, precision: 1, include_unit: true, ...options };
+}
+
 export const DISPLAY_UNIT_KEY = 'dislayUnits';
