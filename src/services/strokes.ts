0276d54150a476b85cae5b309f39a4bdf255eb71
Ryan Chan <ryan@ryanlchan.com>
2023-12-10 09:36:09 -0800

    StrokesService: Change to sync + passed context

--- src/services/strokes.ts
+++ src/services/strokes.ts
@@ -1,4 +1,10 @@
 import { signal } from '@preact/signals';
+import { coordToPoint, formatDistanceAsNumber, formatDistanceOptions, getDistance } from 'common/projections';
+import { clamp, indexSort, touch } from 'common/utils';
+import { CourseFeatureCollection, TERRAIN_TYPES, getTerrainAt } from './courses';
+import { typeid } from 'typeid-js';
+import { getHoleFromRound, roundCourseParams } from './rounds';
+import { GolfClub } from './clubs';
 
 /**
  * ===========
@@ -8,148 +14,126 @@ import { signal } from '@preact/signals';
 
 /**
  * Shows the current position on the map and logs it as a stroke.
- * @param {GeolocationPositionIsh} position - The current geolocation position.
- * @param {object} options - any additional options to set on Stroke
+ * @param position - The current geolocation position.
+ * @param holeIndex - The hole to create the stroke in
+ * @param courseData - The CourseFeatureCollection representing the course
+ * @param round - The round to create within
+ * @param options - any additional options to set on Stroke
  */
-function strokeCreate(position: GeolocationPositionIsh, options: object = {}) {
-    // handle no current hole
-    if (currentHole == undefined) {
-        currentHole = round.holes.reduce((latest, hole) => {
-            return hole.index > latest.index && hole.strokes.length > 0 ? hole : latest
-        })
-        holeSelect(currentHole.index);
-    }
-
-    // Create the stroke object
-    const course = roundCourseParams(round);
+function strokeCreate(
+    position: GeolocationPositionIsh,
+    holeIndex: number,
+    courseData: CourseFeatureCollection,
+    round: Round,
+    options: object = {}) {
+    // TODO: Move to map logic
+    // if (currentHole == undefined) {
+    //     currentHole = round.holes.reduce((latest, hole) => {
+    //         return hole.index > latest.index && hole.strokes.length > 0 ? hole : latest
+    //     })
+    //     holeSelect(currentHole.index);
+    // }
+    const hole = getHoleFromRound(round, holeIndex);
     const stroke: Stroke = {
         id: typeid("stroke").toString(),
-        index: currentHole.strokes.length,
-        holeIndex: currentHole.index,
+        index: hole.strokes.length,
+        holeIndex: hole.index,
         start: {
             x: position.coords.longitude,
             y: position.coords.latitude,
             crs: "EPSG:4326",
         },
-        terrain: courses.getTerrainAt(course, [position.coords.latitude, position.coords.longitude]),
+        terrain: getTerrainAt(courseData, [position.coords.latitude, position.coords.longitude]),
         createdAt: new Date().toISOString(),
         updatedAt: new Date().toISOString(),
         ...options
     };
-    if (currentHole.pin) {
-        stroke.aim = currentHole.pin;
+    if (hole.pin) stroke.aim = hole.pin;
+    if (stroke.index > 0) {
+        const lastStroke = strokeGetLastStroke(stroke, round);
+        if (lastStroke.terrain == TERRAIN_TYPES.PENALTY) lastStroke.aim = stroke.start;
     }
-
-    // Add the stroke to the data layer
-    currentHole.strokes.push(stroke);
-    touch(currentHole, round);
-
-    // Add the stroke to the view
-    strokeMarkerCreate(stroke);
-    rerender();
+    hole.strokes.push(stroke);
 }
 
 /**
  * Create a new stroke for a given club at current position
- * @param {GeolocationPositionIsh} position the locaation to create a stroke at
- * @param {Club} club the club to create a stroke with
+ * @param position - The current geolocation position.
+ * @param holeIndex - The hole to create the stroke in
+ * @param club the club to create a stroke with
+ * @param courseData - The CourseFeatureCollection representing the course
+ * @param round - The round to create within
+ * @param options - any additional options to set on Stroke
  */
-function clubStrokeCreate(position: GeolocationPositionIsh, club: Club) {
+function strokeCreateWithClub(position: GeolocationPositionIsh,
+    holeIndex: number,
+    club: GolfClub,
+    courseData: CourseFeatureCollection,
+    round: Round,) {
     let options = {
         club: club.name,
         dispersion: club.dispersion,
     }
-    if (club.name == "Penalty") options['terrain'] = "penalty";
-    strokeCreate(position, options)
+    if (club.name == "Penalty") options['terrain'] = TERRAIN_TYPES.PENALTY;
+    strokeCreate(position, holeIndex, courseData, round, options)
 }
 
 /**
  * Delete a stroke out of the round
- * @param {Number} holeIndex
- * @param {Number} strokeIndex
+ * @param stroke the stroke to delete
+ * @param round the round to delete from
  */
-function strokeDelete(holeIndex, strokeIndex: number) {
-    console.debug(`Deleting stroke i${strokeIndex} from hole i${holeIndex}`)
-    let hole = round.holes.find(h => h.index === holeIndex);
-    if (hole) {
-        // Delete from data layer
-        hole.strokes.splice(strokeIndex, 1);
-
-        // Reindex remaining strokes
-        hole.strokes.forEach((stroke, index) => stroke.index = index);
-
-        // Update hole
-        touch(hole, round);
-
-        // Rerender views
-        holeViewDelete()
-        holeViewCreate(hole)
-        rerender();
-    }
+function strokeDelete(stroke: Stroke, round: Round) {
+    const hole = strokeGetHole(stroke, round)
+    const strokes = indexSort(hole.strokes);
+    strokes.splice(stroke.index, 1);
+    strokes.forEach((stroke, index) => stroke.index = index);
+    touch(hole, round);
 }
+
+
 
 /**
  * Reorders a stroke within a Hole
- * @param {Number} holeIndex the hole to reorder (1-indexed)
- * @param {Number} strokeIndex the stroke index to reorder (0-indexed)
- * @param {Number} offset movment relative to the current strokeIndex
+ * @param stroke the stroke index to reorder
+ * @param index the new index it should be at
+ * @param round the Round 
  */
-function strokeReorder(holeIndex: number, strokeIndex: number, offset: number) {
-    console.debug(`Moving stroke i${strokeIndex} from hole i${holeIndex} by ${offset}`)
-    const hole = round.holes[holeIndex]
-    const mover = hole.strokes[strokeIndex]
-    if (offset < 0) {
-        offset = Math.max(offset, -strokeIndex)
-    } else {
-        offset = Math.min(offset, hole.strokes.length - strokeIndex - 1)
-    }
-    hole.strokes.splice(strokeIndex, 1)
-    hole.strokes.splice(strokeIndex + offset, 0, mover)
-    hole.strokes.forEach((stroke, index) => stroke.index = index);
-
-    // Update hole
+function strokeReorder(stroke: Stroke, index: number, round: Round) {
+    const hole = strokeGetHole(stroke, round);
+    const strokes = indexSort(hole.strokes);
+    const oldIndex = stroke.index;
+    index = clamp(index, 0, strokes.length - 1);
+    strokes.splice(oldIndex, 1);
+    strokes.splice(index, 0, stroke);
+    strokes.forEach((stroke, index) => stroke.index = index);
     touch(hole, round);
-
-    // Update the map and polylines
-    rerender()
 }
 
 /**
  * Get the distance from this stroke to the next
- * @param {Stroke} stroke
+ * @param {Stroke} stroke the stroke
+ * @param round the Round 
+ * @returns {number} the distance in meters
  */
-function strokeDistance(stroke: Stroke): number {
-    const nextStart = strokeNextStart(stroke)
+function strokeGetDistance(stroke: Stroke, round: Round): number {
+    const nextStart = strokeGetNextStart(stroke, round)
     return getDistance(stroke.start, nextStart);
 }
 
-/**
- * Set the dispersion for a stroke
- * @param {Stroke} stroke the stroke
- * @param {number | string} [val] the value to set the dispersion to
- * @returns {number} the dispersion of this stroke
- */
-function convertAndSetStrokeDispersion(stroke: Stroke, val: number | string): number {
-    const distOpts = { from_unit: displayUnits, to_unit: "meters", output: "number", precision: 3 } as formatDistanceOptions;
-    touch(stroke, strokeHole(stroke), round);
-    stroke.dispersion = formatDistanceAsNumber(val, distOpts);
-    return stroke.dispersion;
-}
-
-function strokeUpdateTerrain(stroke: Stroke, strokeRound?: Round) {
-    if (!strokeRound) strokeRound = round;
-    const course = roundCourseParams(strokeRound);
-    stroke.terrain = courses.getTerrainAt(course, [stroke.start.y, stroke.start.x])
+function strokeUpdateTerrain(stroke: Stroke, round: Round, courseData: CourseFeatureCollection) {
+    stroke.terrain = getTerrainAt(courseData, coordToPoint(stroke.start))
     touch(stroke);
 }
 
 /**
  * Reset a stroke to aim at the pin
  * @param stroke the stroke to reset aim for
+ * @param round the Round 
  * @returns the updated stroke
  */
-function strokeAimReset(stroke: Stroke): Stroke {
-    const hole = strokeHole(stroke);
+function strokeAimReset(stroke: Stroke, round: Round): Stroke {
+    const hole = strokeGetHole(stroke, round);
     stroke.aim = hole.pin;
     touch(stroke, hole, round);
     return stroke;
@@ -158,47 +142,48 @@ function strokeAimReset(stroke: Stroke): Stroke {
 /**
  * Get the hole for a stroke
  * @param stroke the stroke to get the hole for
+ * @param round the Round 
  * @returns the hole for the stroe
  */
-function strokeHole(stroke: Stroke): Hole {
+function strokeGetHole(stroke: Stroke, round: Round): Hole {
     return round.holes[stroke.holeIndex];
 }
 
-function strokeNextStroke(stroke: Stroke): Stroke {
-    let hole = strokeHole(stroke);
+function strokeGetNextStroke(stroke: Stroke, round: Round): Stroke {
+    let hole = strokeGetHole(stroke, round);
     if (!hole || stroke.index == hole.strokes.length) {
         return undefined;
     }
     return hole.strokes[stroke.index + 1];
 }
 
-function strokeLastStroke(stroke: Stroke): Stroke {
-    let hole = strokeHole(stroke);
+function strokeGetLastStroke(stroke: Stroke, round: Round): Stroke {
+    let hole = strokeGetHole(stroke, round);
     if (!hole || stroke.index == 0) {
         return undefined;
     }
     return hole.strokes[stroke.index - 1];
 }
 
-function strokeNextStart(stroke: Stroke): Coordinate {
-    let nextStroke = strokeNextStroke(stroke);
+function strokeGetNextStart(stroke: Stroke, round: Round): Coordinate {
+    let nextStroke = strokeGetNextStroke(stroke, round);
     if (nextStroke) {
         return nextStroke.start;
     }
-    return strokeHole(stroke).pin;
+    return strokeGetHole(stroke, round).pin;
 }
 
-function strokeLastStart(stroke: Stroke): Coordinate {
-    let lastStroke = strokeLastStroke(stroke);
+function strokeGetLastStart(stroke: Stroke, round: Round): Coordinate {
+    let lastStroke = strokeGetLastStroke(stroke, round);
     if (lastStroke) {
         return lastStroke.start;
     }
     return undefined;
 }
 
-function strokeClosestStroke(stroke: Stroke): Stroke {
-    let lastStroke = strokeLastStroke(stroke);
-    let nextStroke = strokeNextStroke(stroke);
+function strokeGetClosestStroke(stroke: Stroke, round: Round): Stroke {
+    let lastStroke = strokeGetLastStroke(stroke, round);
+    let nextStroke = strokeGetNextStroke(stroke, round);
     if (!lastStroke && !nextStroke) {
         return undefined
     } else if (!lastStroke) {
