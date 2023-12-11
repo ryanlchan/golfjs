dd859af7cdb52fa4b2ddbffa851328d0d3dc2b3e
Ryan Chan <ryan@ryanlchan.com>
2023-12-11 14:38:29 -0800

    common/locations: Update to singals

--- src/common/location.ts
+++ src/common/location.ts
@@ -1,56 +1,15 @@
 import * as turf from '@turf/turf';
-import L from 'leaflet';
-import { PositionError } from 'common/errors';
-import { showError, hideError } from 'common/utils';
+import type L from 'leaflet';
+import { MODAL_TYPES, ModalProps } from "./modals";
+import { GeolocatedResult } from 'hooks/useLocation';
+import { disposableEffect } from 'hooks/core';
+import { Signal } from '@preact/signals';
 
 /**
  * =========
  * Utilities
  * =========
  */
-
-let currentPositionEnabled;
-let currentPosition;
-
-/**
- * Get the user's location from browser or cache and continue to watch it
- * @param {boolean} force set to true to skip location cache
- * @returns {Promise} resolves with a GeolocationPositionIsh
- */
-export async function watchLocation(force?: boolean): Promise<any> {
-    // If location is not yet tracked, turn on BG tracking + force refresh
-    if (!(currentPositionEnabled)) {
-        const updatePosition = (position) => currentPosition = position;
-        const logError = (e) => {
-            console.error(e);
-            console.warn("Geolocation error")
-        }
-        const options = { enableHighAccuracy: true, timeout: 5000, maximumAge: 60000 }
-        currentPositionEnabled = navigator.geolocation.watchPosition(updatePosition, logError, options);
-        force = true;
-    }
-    return getLocationOnce(force);
-}
-
-/**
- * Get the user's location from browser or cache
- * @param {boolean} force set to true to skip location cache
- * @returns {Promise} resolves with a GeolocationPositionIsh
- */
-export async function getLocationOnce(force?: boolean): Promise<GeolocationPositionIsh> {
-    return new Promise((resolve, reject) => {
-        const position = currentPositionRead();
-        if (position && !(force)) {
-            resolve(position);
-        } else if (!navigator.geolocation) {
-            // Create a custom position error
-            throw new PositionError("Geolocation is not supported by this browser.", 2);
-        } else {
-            const options = { maximumAge: 60000, timeout: 5000, enableHighAccuracy: true }
-            navigator.geolocation.getCurrentPosition(resolve, reject, options);
-        }
-    });
-}
 
 /**
  * Get the user's location and compare against a condition
@@ -59,14 +18,29 @@ export async function getLocationOnce(force?: boolean): Promise<GeolocationPosit
  * @param {Function} condition
  * @returns {Promise} resolves with a GeolocationPositionIsh-ish
  */
-export async function getLocationIf(condition: Function): Promise<any> {
-    return watchLocation().then((position) => {
-        if (condition(position)) {
-            return position;
-        } else {
-            throw new Error("Failed conditional test");
-        }
-    });
+export async function getLocationIf(
+    gr: GeolocatedResult,
+    condition: (coords: GeolocationPosition) => boolean
+): Promise<any> {
+    if (gr.isGeolocationAvailable.value && condition(gr.raw.value)) {
+        return Promise.resolve(gr.raw.value);
+    } else if (!gr.isGeolocationEnabled) {
+        gr.getPosition();
+        return new Promise((resolve, reject) => {
+            const disposeCb = disposableEffect((dispose) => {
+                if (condition(gr.raw.value)) {
+                    dispose();
+                    resolve(gr.raw.value);
+                }
+            });
+            setTimeout(() => {
+                disposeCb();
+                reject();
+            }, 5000);
+        });
+    } else {
+        return null;
+    }
 }
 
 /**
@@ -74,18 +48,22 @@ export async function getLocationIf(condition: Function): Promise<any> {
  * For example, if the user is way out of bounds
  * @returns {Promise<GeolocationPositionIsh>} the click location as a promise
  */
-export async function getClickLocation(): Promise<GeolocationPositionIsh> {
+export async function getClickLocation(map: L.Map, modalSignal: Signal<ModalProps>): Promise<GeolocationPositionIsh> {
     return new Promise((resolve) => {
-        const error = new PositionError("Click the map to set location", 0);
-        showError(error, -1);
-        mapView.on('click', (e) => {
+        modalSignal.value = {
+            message: "Click the map to set location",
+            type: MODAL_TYPES.WARN,
+            timeout: 10000
+        };
+
+        map.on('click', (e) => {
             const clickPosition = {
                 coords: {
                     latitude: e.latlng.lat,
                     longitude: e.latlng.lng,
                 }
             }
-            hideError();
+            modalSignal.value = null;
             resolve(clickPosition);
         });
     });
@@ -96,11 +74,15 @@ export async function getClickLocation(): Promise<GeolocationPositionIsh> {
  * @param {turf.FeatureCollection} bound
  * @returns {Promise} resolves with a GeolocationPositionIsh-ish
  */
-export async function getLocationWithin(bound: turf.FeatureCollection): Promise<GeolocationPositionIsh> {
-    return getLocationIf((position) => {
+export async function getLocationWithin(
+    gr: GeolocatedResult,
+    bound: turf.FeatureCollection,
+    map: L.Map,
+    modalSignal: Signal<ModalProps>): Promise<GeolocationPositionIsh> {
+    return getLocationIf(gr, (position) => {
         const point = turf.point([position.coords.longitude, position.coords.latitude])
         return turf.booleanWithin(point, bound)
-    }).catch(getClickLocation);
+    }).catch(() => getClickLocation(map, modalSignal));
 }
 
 /**
@@ -108,11 +90,14 @@ export async function getLocationWithin(bound: turf.FeatureCollection): Promise<
  * Only useful because polygonizing the map for turf is a pain
  * @returns {Promise} resolves with a GeolocationPositionIsh-ish
  */
-export async function getLocationOnMap(): Promise<GeolocationPositionIsh> {
-    return getLocationIf((position) => {
-        const userLatLng = L.latLng(position.coords.latitude, position.coords.longitude);
-        return mapView.getBounds().contains(userLatLng)
-    }).catch(getClickLocation);
+export async function getLocationOnMap(
+    gr: GeolocatedResult,
+    map: L.Map,
+    modalSignal: Signal<ModalProps>): Promise<GeolocationPositionIsh> {
+    return getLocationIf(gr, (position) => {
+        const userLatLng = { lat: position.coords.latitude, lng: position.coords.longitude };
+        return map.getBounds().contains(userLatLng)
+    }).catch(() => getClickLocation(map, modalSignal));
 }
 
 
@@ -121,13 +106,12 @@ export async function getLocationOnMap(): Promise<GeolocationPositionIsh> {
  * @param {number} maximumAge the maximum length of time since update to accept
  * @returns {GeolocationPosition}
  */
-export function currentPositionRead(maximumAge = 5000): GeolocationPosition {
+export function currentPositionRead(gr: GeolocatedResult, maximumAge = 5000): GeolocationPosition {
     // Expire current position if beyond timeout (5s)
-    if ((currentPosition?.timestamp < (Date.now() - maximumAge))
-        || (currentPosition?.coords.accuracy > 10)) {
+    if ((gr.timestamp.value < (Date.now() - maximumAge)) || (gr.coords.value.accuracy > 10)) {
         return undefined;
     }
-    return currentPosition;
+    return gr.raw.value;
 }
 
 /**
@@ -135,8 +119,8 @@ export function currentPositionRead(maximumAge = 5000): GeolocationPosition {
  * @param {number} maximumAge the maximum length of time since update to accept
  * @returns {Coordinate}
  */
-export function currentCoordRead(maximumAge = 5000): Coordinate {
-    const pos = currentPositionRead(maximumAge);
+export function currentCoordRead(gr: GeolocatedResult, maximumAge = 5000): Coordinate {
+    const pos = currentPositionRead(gr, maximumAge);
     if (!pos) return undefined;
     return { x: pos.coords.longitude, y: pos.coords.latitude, crs: "EPSG:4326" };
 }
