c80fe88428b207dfa376a40386ff2319c2c56368
Ryan Chan <ryan@ryanlchan.com>
2023-12-10 21:47:29 -0800

    StatsStore: Fix loads not updating signal value

--- src/hooks/useStats.ts
+++ src/hooks/useStats.ts
@@ -1,24 +1,26 @@
 import { RoundStatsCache, fetchStatsCache } from "services/stats";
 import { RoundStateManager } from './roundStore';
-import { CourseStore } from "hooks/useCourse";
+import { CourseStateManager } from "hooks/useCourse";
 import { roundIsPlayed } from "services/rounds";
 import { Store, asyncMutate, store } from "hooks/core";
 import { useMemo } from "preact/hooks";
 import { effect } from "@preact/signals";
 
 
-const statsStore = (roundStore: RoundStateManager, courseStore: CourseStore): Store<RoundStatsCache> => {
+const statsStore = (roundStore: RoundStateManager, courseStore: CourseStateManager): Store<RoundStatsCache> => {
     const s = store({} as RoundStatsCache)
-    const _load = (round: Round): Promise<RoundStatsCache> => {
-        return ((!round || !roundIsPlayed(round)) ?
-            Promise.resolve({} as RoundStatsCache) :
-            fetchStatsCache(roundStore.data.value, courseStore.course));
+    const _load = async (round: Round): Promise<RoundStatsCache> => {
+        if (!round || !roundIsPlayed(round) || Object.keys(courseStore.data.value).length == 0) {
+            return Promise.resolve({} as RoundStatsCache);
+        } else {
+            return fetchStatsCache(roundStore.data.value, courseStore.data.value);
+        }
     }
-    asyncMutate(s, _load);
-    effect(() => _load(roundStore.data.value))
+    const load = async () => asyncMutate(s, () => _load(roundStore.data.value));
+    effect(() => { load() });
     return s;
 }
 
-export const useStats = (roundStore: RoundStateManager, courseStore: CourseStore): Store<RoundStatsCache> => {
+export const useStats = (roundStore: RoundStateManager, courseStore: CourseStateManager): Store<RoundStatsCache> => {
     return useMemo(() => statsStore(roundStore, courseStore), [])
 }
